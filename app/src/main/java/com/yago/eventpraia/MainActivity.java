a511a94ec5675d84bc9e8186d5b98b05b45a70bc
Yago Santos <yagojf27@gmail.com>
2020-04-15 17:29:04 -0300

    “descrição”

--- app/src/main/java/com/yago/eventpraia/MainActivity.java
+++ app/src/main/java/com/yago/eventpraia/MainActivity.java
@@ -2,13 +2,36 @@ package com.yago.eventpraia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  ViewHolder mviewHolder = new ViewHolder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mviewHolder.textDaysLeft = findViewById(R.id.textDaysLeft);
+        this.mviewHolder.textToday = findViewById(R.id.textToday);
+        this.mviewHolder.btnConfirm = findViewById(R.id.btnConfirm);
+
+        //evento de clique
+        this.mviewHolder.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent trocaTela = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(trocaTela);
+            }
+        });
+    }
+
+    public static class ViewHolder{
+        TextView textToday, textDaysLeft;
+        Button btnConfirm;
     }
 }
